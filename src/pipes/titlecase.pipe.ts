c848d06f14128c84b797bac013b977fb13945848
Joe Osborne <joerosborne@gmail.com>
2024-04-13 22:45:59 -0700

    title case issue

--- src/pipes/titlecase.pipe.ts
+++ src/pipes/titlecase.pipe.ts
@@ -3,6 +3,7 @@ import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class TitlecasePipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
+    if (!value) return value;
     const words = value.split(' ');
     const titleCasedWords = words.map((word: string) => {
       return word[0].toUpperCase() + word.slice(1);
