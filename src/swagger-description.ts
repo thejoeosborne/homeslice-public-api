171ee250429f49cc0c31b56be341929fb384c575
Joe Osborne <joerosborne@gmail.com>
2024-04-13 22:38:03 -0700

    typo in swagger description

--- src/swagger-description.ts
+++ src/swagger-description.ts
@@ -10,5 +10,5 @@ export const SWAGGER_DESCRIPTION = `
 <h4>GitHub: <a href="https://github.com/thejoeosborne" target="_blank">github.com/thejoeosborne</a></h4>
 <h4>LinkedIn: <a href="https://www.linkedin.com/in/joe-osborne-profile/" target="_blank">linkedin.com/in/joe-osborne-profile</a></h4>
 <h4>Blog: <a href="https://medium.com/@joerosborne" target="_blank">medium.com/@joerosborne</a></h4>
-<h4>Support me: <a href="https://www.buymeacoffee.com/joeosborne" target="_blank">buymeacoffee.com/joeosbornee</a></h4>
+<h4>Support me: <a href="https://www.buymeacoffee.com/joeosborne" target="_blank">buymeacoffee.com/joeosborne</a></h4>
 `;
